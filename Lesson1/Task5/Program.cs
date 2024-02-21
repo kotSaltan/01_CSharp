f9a75b89b9d36d2a5d484f16de74546c1294e800
Anna Dorosheva <dorosheva.av@gmail.com>
2024-02-21 12:29:02 +0300

    add lesson1;task5

---
+++ Lesson1/Task5/Program.cs
@@ -0,0 +1,10 @@
+﻿int n = 1000;
+int i = 1; // счетчик, начинается с 1 и далее плюс 1
+
+while (i <= n)
+{
+    //Console.WriteLine(i);
+    Console.Write(i);
+    Console.Write(' ');
+    i = i + 1;
+}
