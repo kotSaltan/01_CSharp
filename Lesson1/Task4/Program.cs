dd30c3e2d864d7b3965e1f682627e63bbf110f6b
Anna Dorosheva <dorosheva.av@gmail.com>
2024-02-21 12:15:11 +0300

    add lesson1; task4

---
+++ Lesson1/Task4/Program.cs
@@ -0,0 +1,31 @@
+﻿int a = 116;
+int b = 16;
+int c = 66;
+int d = 65;
+int e = 1126;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+
+}
+if (c > max)
+{
+    max = c;
+
+}
+if (d > max)
+{
+    max = d;
+
+}
+if (e > max)
+{
+    max = e;
+
+}
+
+Console.WriteLine(max);
+
