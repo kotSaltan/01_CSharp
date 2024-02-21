75c9db0d22b6a5fe623e2a68bb04cced7ccfea88
Anna Dorosheva <dorosheva.av@gmail.com>
2024-02-21 12:01:16 +0300

    add lesson1; task 3

--- Lesson1/Task3/Program.cs
+++ Lesson1/Task3/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 50;
+int b = 7;
+
+if (a > b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
